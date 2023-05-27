a84af3a8900ab467f4928ef265c685333d76c8a4
Abhinav Raj <129362857+ARJ010@users.noreply.github.com>
2023-05-27 12:20:48 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
-//import Head from 'next/head'
+import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import {useRouter} from 'next/router'
+import {useState} from "react"
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const router = useRouter();
+  const [name, setName] = useState("");
+  const [place, setPlace] = useState("");
   return (
-    <div>
-    <h1>{router.query.name && router.query.place ?`Hello, ${router.query.name} from ${router.query.place}`:'I dont know your name or place'}</h1>
+  <div>
+    <br />
+    <input type="text" onChange={(event) => setName(event.target.value)} placeholder = "Enter your name"/><br />
+    <br />
+    <input type="text" onChange={(event) => setPlace(event.target.value)} placeholder = "Enter your Place"/>
+    <br />
+    <h1>{name ?`Hai, I am ${name} `:'Hai, I am ____'}</h1>
+    <h1>{place ?`and I am from ${place}`:'and I am from ____'}</h1>
   </div>
 );
 }
